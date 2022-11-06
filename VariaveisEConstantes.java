b018509227e1423d5241480dc5b3cb4b855672ea
Marcus Vinicius <113376779+MarcusPaixaooo@users.noreply.github.com>
2022-11-06 19:05:09 -0300

    Update VariaveisEConstantes.java

--- VariaveisEConstantes.java
+++ VariaveisEConstantes.java
@@ -4,11 +4,13 @@ public class VariaveisEConstantes {
     public static void main(String[] args) {
         //Definindo circunferência de uma área.
         double raio = 3.4;
+        
         //Utilizar constantes com letras maíusculas.
         final double PI = 3.14159;
         double area = PI * raio * raio;
         System.out.println(area);
-    //Alterção no valor da variável.
+        
+        //Alterção no valor da variável.
         raio = 10;
         area = PI * raio * raio;
         System.out.println("Area = " + area + "m2");
